628a90367d39b202ed40cd851008a8ea4f32b3c6
Adam Hardy <ahardy55@gmail.com>
2014-04-16 10:44:45 -0400

    code combat

---
+++ unit1_projects/my_solution.js
@@ -0,0 +1,71 @@
+// Rescue Mission
+// move up x1
+// move right x2
+// move down x1
+// attack
+
+//Grab the Mushroom
+// move right
+// move left
+
+//Drink ME
+// attack
+// move right
+// move down
+// move up
+// move right
+// attack
+
+// Taunt the guards
+// move right
+// say hey there
+// move left x 2
+// attack
+// move right
+// say follow me
+// move right x2
+// move up
+// move right
+// distract guards
+// move down
+// move right
+// move up
+// move right
+
+// It's a Trap
+// move down x2
+// taunt
+// move up x2
+
+// Taunt
+// taunt x3
+
+//Cowardly Taunt
+// move to area where I can be heard
+// taunt
+// move back to safe area
+
+//Commanding followers
+// command them to follow
+// command to attack
+
+// Mobile Artillery
+// move 
+// attack group 1
+// move
+// attack group 2
+
+
+//What is this referring to? Think programming-wise rather than in the terms of the game.
+// It refers to the owner of the function we're executing. 
+
+//What does the () do in JavaScript?
+// Holds functions and parameters
+
+//What is the point of the semicolons?
+// They act like periods, used to separate statements
+
+//Reflection
+//This is a weird one to reflect one.  I actually didn't learn anything from this exercise...I knew a bit of Javascript already, and this one mostly just seemed to be writing the moveRight function with no information as to what went on behind it.  The difficulty was in figuring out the game itself, not so much the actual coding.  No sir... I didn't like it one bit. 
+
+
