c324876e9cd1ee89571ef4f77eccf1124a0d97e9
Adam Hardy <ahardy55@gmail.com>
2014-04-26 14:15:07 -0400

    added cipher challenge

---
+++ unit2_projects/cipher_challenge.rb
@@ -0,0 +1,92 @@
+# U2.W4: Cipher Challenge
+
+
+# I worked on this challenge with: .
+
+
+
+# 1. Solution
+# Write your comments on what each thing is doing. 
+# If you have difficulty, go into IRB and play with the methods.
+
+
+
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
+  decoded_sentence = []
+  alphabet = ('a'..'z').to_a #setting variable to store alphabet, this can be used later for cipher
+  symbols = ['@', '#', '$', '%', '^', '&', '*'] #variable used to store symbols
+  cipher = alphabet.rotate(-4)
+            
+  input.each do |x| # What is #each doing here?
+    found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
+    cipher.each_key do |y| # What is #each_key doing here?
+      if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
+      #  puts "I am comparing x and y. X is #{x} and Y is #{y}."
+        decoded_sentence << cipher[y]
+        found_match = true
+        break  # Why is it breaking here?
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+        decoded_sentence << x
+        found_match = true
+        break
+      end 
+    end
+    if not found_match  # What is this looking for?
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+ 
+  if decoded_sentence.match(/\d+/) #What is this matching? Look at Rubular for help. 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...
+  end  
+  return decoded_sentence # What is this returning?        
+end
+
+# Your Refactored Solution
+
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") 
+  decoded_sentence = [] 
+  alphabet = ('a'..'z').to_a 
+  symbols = ['@', '#', '$', '%', '^', '&', '*'] 
+  cipher = alphabet.rotate(-4)  
+ 
+  input.each do |x| 
+    if alphabet.include?(x)  
+      decoded_sentence << cipher[alphabet.index(x)]  
+    elsif symbols.include?(x) 
+      decoded_sentence << " " 
+    else
+      decoded_sentence << x 
+    end
+  end
+ 
+  decoded_sentence = decoded_sentence.join("") 
+ 
+  if decoded_sentence.match(/\d+/) 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } 
+  end  
+  decoded_sentence  
+
+end
+
+
+
+# Driver Code:
+p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
+# Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms." 
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")== "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
+
+
+# Reflection
+# It wasn't difficult to figure out what the code was doing, but all the excess did make it a bit confusing. Breaking it down line by line made it pretty easy to decipher. Refactoring was a challenge at first, but then we decided it would just be easier to scrap that long, confusing loop entirely and just re-write one from scratch.  Our biggest problem turned out to be just a small error...adding turning our alphabet variable into an array.  Once that was cleared up, the code ran well.  I was able to brush up a bit on regex and the .rotate method, as well as using .include for an actual purpose other than playing around. 
